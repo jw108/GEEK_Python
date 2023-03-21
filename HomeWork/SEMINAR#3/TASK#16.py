30797ec6b3363b8b17e417bc3e85a7f619f114a1
Chugunov <jw108mac@gmail.com>
2023-03-21 18:40:34 +0300

    Добавил решение ДЗ TASK#18

--- HomeWork/SEMINAR#3/TASK#16.py
+++ HomeWork/SEMINAR#3/TASK#16.py
@@ -2,6 +2,24 @@
 Требуется вычислить, сколько раз встречается некоторое
 число X в массиве A[1..N]. Пользователь в первой строке вводит
 натуральное число N – количество элементов в массиве. В последующих
-строках записаны N целых чисел Ai
-. Последняя строка содержит число X
+строках записаны N целых чисел Ai. Последняя строка содержит число X
 """
+from random import randint
+
+n = int(input("Введите размер массива: "))
+array = []
+for i in range(n):
+    array.append(randint(1, 30))
+print(array)
+    
+
+counts = {}
+for num in array:
+    if num in counts:
+        counts[num] += 1
+    else:
+        counts[num] = 1
+
+for num, count in counts.items():
+    if count > 1:
+        print(f"Число {num} повторяется {count} раз(а)")
